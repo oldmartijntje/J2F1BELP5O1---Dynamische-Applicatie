2547a8881bcc7a204da551f553c0483d4da5397f
oldmartijntje <oldmartijntje@gmail.com>
2023-05-25 12:05:03 +0200

    protection against bad input

--- vormgeving/pages/character.php
+++ vormgeving/pages/character.php
@@ -11,29 +11,27 @@
 <?php
 require_once "resources/config.php";
 
-$sql = "SELECT * FROM characters WHERE id = " . $_GET['id'] . "";
-// $sql = "SELECT * FROM characters WHERE id = " . "1; DROP DATABASE;". "";
+$id = $_GET['id'];
+$sql = "SELECT * FROM characters WHERE id = ?";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $id); // Assuming 'id' is an integer (yes it is, thanks GPT :)
 
-    if($result = $mysqli->query($sql)){
-        if($result->num_rows > 0){
-            while($row = $result->fetch_array()){
-            
-                $newDict = [];
-                foreach($row as $key=>$value) {
-                    $newDict[$key] = stripslashes(trim(HTMLspecialchars($value)));
-                    $newDict[$key] = str_replace('\'', '', $newDict[$key]);
-                } 
-                // Loop through each property in the object
-                foreach ($newDict as $key => $value) {
-                    // If the property value is a string containing unescaped "\r" characters
-                    if (is_string($value) && strpos($value, "\r") !== false) {
-                        // Replace unescaped "\r" characters with escaped "\r" characters
-                        $newDict[$key] = str_replace("\r\n", " ", $value);
-                    }
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_array()) {
+            $newDict = [];
+            foreach ($row as $key => $value) {
+                $newDict[$key] = stripslashes(trim(htmlspecialchars($value)));
+                $newDict[$key] = str_replace('\'', '', $newDict[$key]);
+            }
+            foreach ($newDict as $key => $value) {
+                if (is_string($value) && strpos($value, "\r") !== false) {
+                    $newDict[$key] = str_replace("\r\n", " ", $value);
                 }
-                
+            }
     }
-        $result->free();
+    $result->free();
     } else{
         echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
         ?>
